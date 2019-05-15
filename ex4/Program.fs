95aec47b0a10451deafb4aa1d34970756ef8efcc
Irlirion <grigorjew.rostislaw@gmail.com>
2019-05-15 10:02:32 +0300

    Add first

---
+++ ex4/Program.fs
@@ -0,0 +1,15 @@
+﻿// 7.1.1
+let rec fibo = function
+| 0 -> 0
+| 1 -> 1
+| n -> fibo(n - 1) + fibo(n - 2)
+
+// 7.1.2
+let rec sum = function
+| 0 -> 0
+| n -> sum(n - 1) + n
+
+// 7.1.3
+let rec sum2 = function 
+ | (m,0) -> m 
+ | (m,n) -> sum2(m, n - 1) + m + n
