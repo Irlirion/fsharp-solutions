b9ada7151b8d9def50833cd3a635454a321af8c9
Irlirion <grigorjew.rostislaw@gmail.com>
2019-05-15 18:25:35 +0300

    Add

---
+++ ex6/Program.fs
@@ -0,0 +1,12 @@
+﻿// 17.1
+let rec pow = function
+| (s, 1) -> s
+| (s, n) -> s + pow(s, n - 1)
+
+// 17.2
+let rec isIthChar (s: string, n, c) = s.[n] = c
+
+// 17.3
+let rec occFromIth (s, n, c) =
+    if n >= String.length s then 0
+    else (if isIthChar(s, n, c) then 1 else 0) + occFromIth(s, (n + 1), c)  
