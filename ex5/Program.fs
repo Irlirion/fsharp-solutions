55508a75f4eab83d615289f65e3c6c96fa48839a
Irlirion <grigorjew.rostislaw@gmail.com>
2019-05-15 15:42:55 +0300

    Add

---
+++ ex5/Program.fs
@@ -0,0 +1,10 @@
+﻿// 16.1
+let notDivisible (n, m) = m % n = 0
+
+// 16.2
+let  prime  n =
+    let mutable flag = true
+    for i = 2 to n - 1 do
+         if notDivisible(i, n) then
+             flag <- false
+    flag
