b4d7d7d649391dddea2ca5d84d117690cb183991
Irlirion <grigorjew.rostislaw@gmail.com>
2019-05-16 10:44:48 +0300

    Add

---
+++ ex7/Program.fs
@@ -0,0 +1,10 @@
+﻿// 20.3.1
+let vat n x = x + float n * x / 100.
+
+// 20.3.2
+let unvat n x = x * 100. /  float (n + 100)
+
+// 20.3.3
+let rec min f = fun n ->
+    if f(n) = 0 then n
+    else min f (n + 1)
