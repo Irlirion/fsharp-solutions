e24c5fc2149e0fcc9ff3c88a09516a915eb00ca9
Irlirion <grigorjew.rostislaw@gmail.com>
2019-05-16 15:31:26 +0300

    Make min recursive

--- ex7/Program.fs
+++ ex7/Program.fs
@@ -2,11 +2,11 @@
 let vat n x = x + float n * x / 100.
 
 // 20.3.2
-let unvat n x = x * 100. /  float (n + 100)
+let unvat n x = x * 100. / float (n + 100)
 
 // 20.3.3
 let rec min f =
-    let mutable n = 0
-    while f(n) <> 0 do
-        n <- n + 1
-    n
+    let rec iter_min (f, n) =
+        if f (n) = 0 then n
+        else iter_min (f, n + 1)
+    iter_min (f, 0)
