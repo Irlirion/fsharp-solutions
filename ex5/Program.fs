b612b43dbbe5b50e748e2f1f49db3347c6e62a8b
Irlirion <grigorjew.rostislaw@gmail.com>
2019-05-16 15:25:56 +0300

    Make prime recursive

--- ex5/Program.fs
+++ ex5/Program.fs
@@ -2,9 +2,10 @@
 let notDivisible (n, m) = m % n = 0
 
 // 16.2
-let  prime  n =
-    let mutable flag = true
-    for i = 2 to n - 1 do
-         if notDivisible(i, n) then
-             flag <- false
-    flag
+let prime n =
+    let rec iter_prime (n, i) =
+        if i >= n then true
+        elif notDivisible (i, n) then
+            false
+        else iter_prime (n, i + 1)
+    iter_prime (n, 2)
