fd183191080b7536e3abd8cdc25ac05eeb25f1e8
Irlirion <grigorjew.rostislaw@gmail.com>
2019-05-20 20:14:53 +0300

    Add ex15

---
+++ ex15/Program.fs
@@ -0,0 +1,8 @@
+﻿// 41.4.1
+let list_filter f xs = List.foldBack (fun head tail -> if f head then head :: tail else tail) xs []
+
+// 41.4.2
+let sum (p, xs) = List.fold (+) 0 (List.filter p xs)
+
+// 41.4.3
+let revrev = fun lst -> List.fold (fun head tail -> List.rev tail :: head) [] lst
