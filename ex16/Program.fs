361d9ba302d2b2fae511e1fa3006e41d7e1fc128
Irlirion <grigorjew.rostislaw@gmail.com>
2019-05-21 13:32:51 +0300

    Add ex16

---
+++ ex16/Program.fs
@@ -0,0 +1,7 @@
+﻿// 42.3
+let rec allSubsets n k =
+    let rec powerset xs=
+        match xs with
+        | [] -> [[]]
+        | head::tail -> List.fold (fun head1 tail1 -> (head::tail1)::tail1::head1) [] (powerset tail)
+    Set.filter (fun xs -> Set.count xs = k) (Set.ofList (List.map Set.ofList (powerset [1 .. n])))
