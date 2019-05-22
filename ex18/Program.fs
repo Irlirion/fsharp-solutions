07741591070e827f0bf770b42d354855b77496ae
rostislav <grigorjew.rostislaw@gmail.com>
2019-05-22 10:09:55 +0300

    Add ex18

---
+++ ex18/Program.fs
@@ -0,0 +1,18 @@
+﻿// 47.4.1
+let f n =
+    let mutable mul = 1
+    let g x = mul <- mul * x
+    List.iter g [1 .. n]
+    mul
+
+// 47.4.2
+let fibo n =
+    let mutable fib1 = 0
+    let mutable fib2 = 1
+    let mutable fibonacci = fib1
+    let f x =
+        fibonacci <- fib1 + fib2
+        fib1 <- fib2
+        fib2 <- fibonacci
+    List.iter f [2.. n]
+    fibonacci
