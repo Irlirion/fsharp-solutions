29be2e1899c356fb4c6c5504111155511129e105
rostislav <grigorjew.rostislaw@gmail.com>
2019-05-23 16:20:37 +0300

    Fix

--- ex19/Program.fs
+++ ex19/Program.fs
@@ -1,8 +1,8 @@
 ﻿// 48.4.1
 let rec fibo1 n n1 n2 =
     match n with
-    | 0 -> n1
-    | n -> fibo1 (n - 1) n2 (n1 + n2) 
+    | 0 -> n2
+    | n -> fibo1 (n - 1) (n1 + n2) n1
 
 // 48.4.2
 let rec fibo2 c n =
