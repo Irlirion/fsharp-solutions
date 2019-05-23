c401cb7f7a5c4e09e647c4095e7e61646137a7e7
rostislav <grigorjew.rostislaw@gmail.com>
2019-05-23 20:25:56 +0300

    Add ex22

---
+++ ex22/Program.fs
@@ -0,0 +1,19 @@
+﻿type 'a cell = Nil | Cons of 'a * Lazy<'a cell>
+
+let hd (s : 'a cell) : 'a =
+  match s with
+    Nil -> failwith "hd"
+  | Cons (x, _) -> x
+
+let tl (s : 'a cell) : Lazy<'a cell> =
+  match s with
+    Nil -> failwith "tl"
+  | Cons (_, g) -> g
+
+
+// 51.3
+let rec nth (s : 'a cell) (n : int) : 'a = if n = 0 then hd s else nth ((tl s).Force()) (n - 1)
+  
+
+// например, получить 30000-й элемент:
+// nth n0 30000
