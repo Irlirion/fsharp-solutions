bc3c669e22276b1c2067c3825b17b71cf3ed6872
rostislav <grigorjew.rostislaw@gmail.com>
2019-05-23 18:58:39 +0300

    Fix

--- ex21/Program.fs
+++ ex21/Program.fs
@@ -1,22 +1,22 @@
 ﻿// 50.2.1
-let fac_seq =
+let fac_seq n =
     let factorial n =
         let rec f x a =
             if x <= 1 then a
             else f (x - 1) (a * x)
         f n 1
-    fun n -> seq {
+    seq {
         for i in 0 .. n do
             yield factorial i
     }
 
 // 50.2.2
-let seq_seq =
+let seq_seq n =
     let f n =
         match n with
         | n when n % 2 = 0 -> n / 2
         | n -> - (n + 1) / 2
-    fun n -> seq {
+    seq {
         for i in 0 .. n do
             yield f i
     }
