7e0f296e660ba475b9b60536dd63585083cfc592
rostislav <grigorjew.rostislaw@gmail.com>
2019-05-23 17:50:27 +0300

    Add ex21

---
+++ ex21/Program.fs
@@ -0,0 +1,22 @@
+﻿// 50.2.1
+let fac_seq =
+    let factorial n =
+        let rec f x a =
+            if x <= 1 then a
+            else f (x - 1) (a * x)
+        f n 1
+    fun n -> seq {
+        for i in 0 .. n do
+            yield factorial i
+    }
+
+// 50.2.2
+let seq_seq =
+    let f n =
+        match n with
+        | n when n % 2 = 0 -> n / 2
+        | n -> - (n + 1) / 2
+    fun n -> seq {
+        for i in 0 .. n do
+            yield f i
+    }
